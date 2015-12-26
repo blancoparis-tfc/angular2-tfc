e17b003684a6ed122ed29520bdccdec42037a565
blancoparis-tfc <blancoparis.tfc@gmail.com>
2015-12-27 00:12:39 +0100

    Operacion para confirmar una ventana modal
    closes #8
    
    Hemos creado, una funcional de ventana de confirmar. En la operativa.

--- src/app/components/ejemplos/modal/modal.component.ts
+++ src/app/components/ejemplos/modal/modal.component.ts
@@ -1,32 +1,27 @@
-import {Component,DynamicComponentLoader,ElementRef} from 'angular2/core';
+import {Component,ElementRef} from 'angular2/core';
 import {ModalDirective} from '../../../directives/comun/modal/modal.directive'
 import {BlockComponent} from '../../comun/block.component';
-import {DbpDialogo,DbpDialogoAlertConf} from '../../../core/modal/dialogo';
+import {DbpDialogo,DbpDialogoAlertConf,DbpDialogoConfirmarConf} from '../../../core/modal/dialogo';
 @Component({
   selector:'ejemplo-modal',
   templateUrl:'/src/app/components/ejemplos/modal/modal.html',
   directives:[ModalDirective]
 })
 export class ModalComponent{
-//DynamicComponentLoader
-  constructor(
-     private cargadorDeComponentes:DynamicComponentLoader
-    ,private elemento:ElementRef
-    ,private dialogo:DbpDialogo
-  ){
 
-  }
-
-  procesarOk(){
-    console.info('Le dio al ok');
-  }
+  constructor(private elemento:ElementRef,private dialogo:DbpDialogo){}
 
   abrirModal(){
-    console.info('Abrir una ventana modal 21');
-    //this.cargadorDeComponentes.loadNextToLocation(BlockComponent,this.elemento);
-    this.dialogo.alert(this.elemento,new DbpDialogoAlertConf('Mensaje','Boton Ok','Titulo')).then(dialogoRef=>{
-      console.info('Se ha procesado el alert');
+    this.dialogo.alert(this.elemento,new DbpDialogoAlertConf('Mensaje','Titulo')).then(dialogoRef=>{
+      dialogoRef.cuandoCerramos.then((_)=>{console.info('Se ha cerrado el alert')});
       return dialogoRef;
+    });
+  }
+
+  abrirConfirmar(){
+    this.dialogo.confirmar(this.elemento,new DbpDialogoConfirmarConf('Mensaje de confirmar','Ejemplo de confirmar')).then(dialogoComponent=>{
+        dialogoComponent.instance.cuandoOk.then((_)=>{console.info(' despues Ok 234');});
+        dialogoComponent.instance.cuandoCancelar.then((_)=>{console.info(' despues cancelar 234');});
     });
   }
 }
--- src/app/core/modal/dialogo.ts
+++ src/app/core/modal/dialogo.ts
@@ -27,7 +27,7 @@ export class DbpDialogo{
       var dbpDialogoRef:DbpDialogoRef=new DbpDialogoRef();
       var ocultarPromesa=  this.cargador.loadNextToLocation(BlockDialogoComponent,elemento);
       return this.cargador.loadNextToLocation(DialogoAlertComponent,elemento).then(containerRef =>{
-        console.info('Antes de entrar',containerRef);
+        console.info('Antes de entrar',containerRef,dialogoConf);
         dbpDialogoRef.contentRef=containerRef;
         containerRef.instance.configuacion=dialogoConf;
         containerRef.instance.dbpDialogoRef=dbpDialogoRef;
@@ -38,15 +38,31 @@ export class DbpDialogo{
       });
   }
 
+  public confirmar(elemento:ElementRef,dialogoConf:DbpDialogoConfirmarConf):Promise<ComponentRef>{
+      var dbpDialogoRef:DbpDialogoRef=new DbpDialogoRef();
+      var ocultarPromesa=  this.cargador.loadNextToLocation(BlockDialogoComponent,elemento);
+      return this.cargador.loadNextToLocation(DialogoConfirarComponent,elemento).then(containerRef =>{
+        console.info('Antes de entrar',containerRef,dialogoConf);
+        dbpDialogoRef.contentRef=containerRef;
+        containerRef.instance.configuacion=dialogoConf;
+        containerRef.instance.dbpDialogoRef=dbpDialogoRef;
+        ocultarPromesa.then(ocultarRef=>{
+          dbpDialogoRef.cuandoCerramos.then((_)=>{ocultarRef.dispose();})
+        })
+        return containerRef;
+      });
+  }
 }
 /**
 * Es la referencia a un dialogo abierto.
 **/
 export class DbpDialogoRef{
+
     _contentRef:ComponentRef; // Representa al componente de la ventana.
     private isCerrado:Boolean; // Nos indica se venta se ha cerrado.
     contentRefDeferred:any; // Lo usaremos, para sincronizar los datos.
     cuandoCerramosDeferred:any; // Se lanzara cuando se cierre la ventana
+
     constructor(){
       this._contentRef = null;
       this.isCerrado = false;
@@ -77,19 +93,17 @@ export class DbpDialogoRef{
 
 }
 
-
 export class DbpDialogoAlertConf{
   constructor(
     public mensaje:String,
-    public mensajeOk:String,
     public titulo:String = ''
   ){
 
   }
 }
 @Component({
-  selector:'dbp-dialogo',
-  templateUrl:'/src/app/core/modal/dialogo.html',
+  selector:'dbp-dialogo-alert',
+  templateUrl:'/src/app/core/modal/dialogo.alert.html',
   host:{
       '(body:keydown)':'documentKeypress($event)'
   }
@@ -101,10 +115,65 @@ export class DialogoAlertComponent{
       this.dbpDialogoRef.cerrar();
   }
   documentKeypress(event:KeyboardEvent){
-
     if (event.keyCode == KeyCodes.ESCAPE) {
       this.dbpDialogoRef.cerrar();
     }
+  }
+}
+
+
+export class DbpDialogoConfirmarConf{
+  constructor(public mensaje:String = '',public titulo:String ='',
+    public botonOk:String ='Ok',public botonCancelar:String ='Candelar' ){ }
+}
+
+@Component({
+  selector:'dbp-dialogo-confirmar',
+  templateUrl:'/src/app/core/modal/dialogo.confirmar.html',
+  host:{
+      '(body:keydown)':'documentKeypress($event)'
+  }
+})
+export class DialogoConfirarComponent{
+  public configuacion:DbpDialogoConfirmarConf;
+  public dbpDialogoRef:DbpDialogoRef;
+  public okDeferred:any;
+  public cancelarDeferred:any;
+  constructor(){
+    this.okDeferred=PromiseWrapper.completer();
+    this.cancelarDeferred=PromiseWrapper.completer();
+  }
+  cerrarModal(){
+      this.dbpDialogoRef.cerrar();
+  }
+
+  documentKeypress(event:KeyboardEvent){
+    if (event.keyCode == KeyCodes.ESCAPE) {
+      this.dbpDialogoRef.cerrar();
+    }
+  }
+
+  get cuandoOk():Promise<any>{
+    return this.okDeferred.promise;
+  }
+
+  get cuandoCancelar():Promise<any>{
+    return this.cancelarDeferred.promise;
+  }
+
+  procesarOk(elemento=null){
+      console.info('Operacion OK');
+        this.okDeferred.resolve(elemento);
+        this.dbpDialogoRef.cuandoCerramos.then((_)=>{
+      });
+      this.dbpDialogoRef.cerrar();
+  }
+  procesarCancelar(elemento=null){
+      console.info('Operación cancelar');
+      this.dbpDialogoRef.cuandoCerramos.then((_)=>{
+        this.cancelarDeferred.resolve(elemento);
+      });
+      this.dbpDialogoRef.cerrar();
   }
 }
 
